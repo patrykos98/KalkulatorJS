e8645e251b0044a05ef10665646c33b543b75b9f
Patryk Szczemczak <patrykszczemczak@wp.pl>
2023-12-17 20:49:40 +0100

    poczatki onp i poczatek konca kalkulatora

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const buttons=document.querySelectorAll('button');
 const wynik=document.querySelector('div p');
 
+
 buttons.forEach(function(btn){
    btn.addEventListener('click',function(el){
     if(el.target.className==='ac'){
@@ -10,9 +11,21 @@ buttons.forEach(function(btn){
         tablicaZnakow.pop();
         wynik.innerText=tablicaZnakow.join('');
     }else if(el.target.className==='wynik_btn'){
-        //let tablicaDoDzialan=[...wynik.innerText];
-        //console.log(tablicaDoDzialan);
-        console.log(wynik.innerText);
+        let wynikPrzedObliczeniem=wynik.innerText+'=';
+        let tablicaDoDzialan=[...wynik.innerText];
+        let wynikLiczba;
+        
+        function funkcjaTworzącaLiczbe(){
+            
+           tablicaDoDzialan.forEach(function(el){
+            while(el!='*' & el!='/' & el!='^' & el!='+' & el!='-'){
+                
+            }
+        })
+
+        }
+        //let iloscRownan=(tablicaDoDzialan.filter(el => el!=='1' & el!=='2' & el!=='3' & el!=='4' & el!=='5' & el!='6' & el!='7' & el!='8' & el!='9' & el!='0')).length;
+
         // tutaj wynik dzialania
     }
     else{
